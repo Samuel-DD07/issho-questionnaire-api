668bcbf6bd71cc2ddad02c63748b8e67636d2474
Samuel Dorismond <samuel.dorismond@yahoo.com>
2022-10-19 21:12:34 +0200

    ok

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5001
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api', (req, res) => {
+    res.send('Hello World!')
+});
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
